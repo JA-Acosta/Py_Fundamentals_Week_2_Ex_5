73531ea5a59c5ce8010dc8b5c1cf9bcec9a91684
JAAR <joe.aaron.acosta@outlook.com>
2023-07-24 22:08:54 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 07/24/2023
+>>> Practicing Fundamentals Program 11
+>>> Version 0
+'''
+
+'''
+>>> Generates a program that will create a random password for the user based on the parameters provided by the user.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
